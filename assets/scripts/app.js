bf56b2bddc2d6fb3ad7a8c032a002e2cf41e387a
dvdpwll <dvdpwll456@gmail.com>
2016-08-22 15:36:01 -0400

    moved all event handlers
    moved all event handlers outside of the functions to prevent multiple
    requests being made on one click.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,8 +2,8 @@
 
 const app = {
   //api: 'http://httpbin.org',
-  // api: 'http://localhost:3000/',
-  api: 'https://murmuring-harbor-68088.herokuapp.com/',
+  api: 'http://localhost:3000/',
+  // api: 'https://murmuring-harbor-68088.herokuapp.com/',
   elements: [],
 };
 
