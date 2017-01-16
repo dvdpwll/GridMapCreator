e1d2b9a26e87544df7a718005e51af5ce2b73d3c
dvdpwll <dvdpwll456@gmail.com>
2017-01-16 17:46:39 -0500

    fixing app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,12 +2,9 @@
 
 const app = {
   //api: 'http://httpbin.org',
-<<<<<<< HEAD
   api: 'https://murmuring-harbor-68088.herokuapp.com/',
-=======
   elements0: [],
   elements1: [],
->>>>>>> master
 };
 
 module.exports = {
